d626f28f964c132e52579d08d36ffeb30f6dbc16
twonehope <85345965+twonehope@users.noreply.github.com>
2021-12-12 16:56:13 +0000

    Add files via upload

---
+++ Python7.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("ls")
+print("\n")
+os.system("mkdir Testing")
+print("\n")
+os.system("ls")
+print("\n")
+os.system("sudo rmdir Testing")
+print("\n")
+os.system("ls")
+print("\n")
+
